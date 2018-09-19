595046354fb1678d4ec339adb77f685f378f5303
lakenqi <33916890+lakenqi@users.noreply.github.com>
2018-09-19 13:23:59 +0800

    2018.9.19

--- Vue.easyUtil.js
+++ Vue.easyUtil.js
@@ -5,7 +5,7 @@
 /* = CopyRight by qy  = */
 /* ========== */
 
-//默认值*****************************************************
+//==============================默认值========================================
 var _s = Vue.prototype._s
 Vue.prototype._s = function (s) {
   if(typeof s === 'number'){
@@ -14,8 +14,8 @@ Vue.prototype._s = function (s) {
   	return _s.call(this, s || "--")
   } 
 }
-//默认值结束***************************************************
-//组件部分*****************************************************
+//====================================默认值结束====================================
+//====================================组件部分====================================
 //  = 下拉框 = 
 Vue.component('selected',{
 	props : ['id','text'],
@@ -84,7 +84,6 @@ Vue.component('super-table',{
 		<div :id="bodyId" :class="bodyClass">\
 			<div :id="tbodyId" class="easyUtil-table" v-cloak>\
 				<div class="tbody" :class="bodyCounter">\
-					<div id="hiddenNoSee" class="tr easyUtil-hidden"></div>\
 					<slot name="tbody"></slot>\
 				</div>\
 			</div>\
@@ -111,7 +110,7 @@ Vue.component('super-table',{
 			tbodyId : this.body,
 			mainClass : this.maindivclass,
 			bodyClass : this.bodydivclass,
-			imgPath : this.loadingpath || "../img/Loading6-3.gif",
+			imgPath : this.loadingpath || "../images/Loading6-3.gif",
 			/*noContent :　this.emptytip || "暂无数据",*/
 			showHead : false,
 			counterClass : this.iscounter,
@@ -129,9 +128,158 @@ Vue.component('super-table',{
 		}
 	},
 });
+//  = 分页组件 = 
+Vue.component("super-page",{
+	template:'\
+	<div class="easyUtil-page">\
+	  <span v-if="show==0">共 {{totalPage}} 页</span>\
+		<span v-if="show==1">共 {{totalNum}} 条数据</span>\
+		<span class="btn" @click="pageDown" :disabled="currentPage <= min"> << </span>\
+		<span>{{currentPage}}</span>\
+		<span class="btn" @click="pageUp" :disabled="currentPage >= max"> >> </span>\
+		<input type="text" :value="currentPage" @change="pageChange" @blur="goToPage" @keyup.enter="goToPage">\
+	</div>',
+	props:{
+		min:{    //最小页码
+			type:Number,
+			default:1
+		},
+		max:{    //最大页码
+			type:Number,
+			default:Infinity
+		},
+		nums:{    //最大数据量
+			type:Number,
+			default:Infinity
+		},
+		value:{   //当前页码
+			type:Number,
+			default:1
+		},
+		show:{   //显示样式0为总页码,1为总数据
+			type:String,
+			default:"0"
+		}
+	},
+	data:function(){
+		return {
+			totalNum : this.nums,
+			totalPage : this.max,
+			currentPage : this.value,
+			inputId: this.pageid
+		}
+	},
+	watch:{
+		currentPage:function(data){
+			this.$emit('input',data);
+			this.$emit('on-change',data);
+		},
+		value: function(data){
+			this.changeNum(data);
+		}
+	},
+	methods:{
+		pageDown:function(){
+			if(this.currentPage <= this.min){
+				return;
+			}
+			this.currentPage -= 1;
+			this.$emit('down',this.currentPage);
+		},
+		pageUp:function(){
+			if(this.currentPage >= this.max){
+				return;
+			}
+			this.currentPage += 1;
+			this.$emit('up',this.currentPage);
+		},
+		goToPage: function(){
+			this.$emit('go',this.currentPage);
+		},
+		changeNum:function(data){
+			if(data < this.min){
+				data = this.min;
+			}
+			if(data > this.max){
+				data = this.max;
+			}
+		},
+		pageChange:function(e){
+			var val = e.target.value.trim(),
+					max = this.max,
+					min = this.min,
+					regex = /^[0-9]*$/;
+			if(regex.test(val)){
+				val = parseInt(val,10);
+				this.currentPage = val;
+				if(val > max){
+					this.currentPage = max;
+				}else if(val < min){
+					this.currentPage = min;
+				}
+			}else{
+				e.target.value = this.currentPage;
+			}
+		}
+	},
+	mounted:function(){
+		this.changeNum(this.value);
+	}
+});
 //  ========== 
-//组件部分结束*****************************************************
-//指令部分********************************************************
+//  = div生成横向bar图带数字 =
+Vue.component('super-bar',{
+	template:'\
+	<div class="easyUtil-outBar">\
+		<div class="easyUtil-bar" :style="styles"></div>\
+		<span>{{val}}</span>\
+	</div>',
+	props:["bar","value"],
+	data:function(){
+		return {
+			barWidth:this.bar,
+			val :this.value
+		}
+	},
+	computed:{
+		styles:function(){
+			return {"width": this.barWidth+"%"};
+		},
+	}
+});
+//  ========== 
+//  ========== 
+//  = 进度条组件 名称 数字跟随 显示百分比 = 
+Vue.component('super-progressbar',{
+	template:'\
+	<div class="easyUtil-progressBar">\
+		<div>{{type}}</div>\
+		<div class="easyUtil-numDiv" :class="colorClass">\
+			<div :style="styles">{{val}}</div>\
+		</div>\
+		<div>{{width}}</div>\
+		<div class="easyUtil-outProBar">\
+			<div class="easyUtil-inProBar" :class="colorClass" :style="styles"></div>\
+		</div>\
+	</div>',
+	props:["name","value","color","percent"],
+	data:function(){
+		return {
+			width:this.percent,
+			val :this.value,
+			colorClass: this.color,
+			type : this.name
+		}
+	},
+	computed:{
+		styles:function(){
+			return {"width": this.width};
+		},
+	}
+});
+//  ========== 
+//====================================组件部分结束====================================
+//====================================指令部分====================================
 //  = 自定义外部点击关闭指令 = 
 Vue.directive('outsideclose',{
 	bind: function (el,binding,vnode) {
@@ -152,4 +300,4 @@ Vue.directive('outsideclose',{
 	}
 });
 //  ========== 
-//指令部分结束********************************************************
+//====================================指令部分结束====================================
